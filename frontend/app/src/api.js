e36751455becc6b091afb7927cd195099e780937
Exillite <greencod67@gmail.com>
2023-04-04 11:45:45 +0300

    add CRUD reqests methods for all modules

--- frontend/app/src/api.js
+++ frontend/app/src/api.js
@@ -50,4 +50,80 @@ export default {
     async delete_user(user_id) {
         return axios.delete(`/user/${user_id}`);
     },
+
+
+    async create_new_student_group(title, teacher_id) {
+        return axios.post('/student_group', {
+            title: title,
+            teacher_id: teacher_id,
+        });
+    },
+
+    async get_student_group(student_group_id) {
+        return axios.get(`/student_group/${student_group_id}`);
+    },
+
+    async edit_student_group(student_group_id, title) {
+        return axios.put(`/student_group/${student_group_id}`, {
+            title: title,
+        });
+    },
+
+    async delete_student_group(student_group_id) {
+        return axios.delete(`/student_group/${student_group_id}`);
+    },
+
+
+    async create_new_homework(title, file, student_group_id, deadline, points, mark_formula) {
+        return axios.post('/homework', {
+            title: title,
+            file: file,
+            student_group_id: student_group_id,
+            deadline: deadline,
+            points: points,
+            mark_formula: mark_formula,
+        });
+    },
+
+    async get_homework(homework_id) {
+        return axios.get(`/homework/${homework_id}`);
+    },
+
+    async edit_homework(homework_id, title, file, deadline, points, mark_formula) {
+        return axios.put(`/homework/${homework_id}`, {
+            homework_id: homework_id,
+            title: title,
+            file: file,
+            deadline: deadline,
+            points: points,
+            mark_formula: mark_formula,
+        });
+    },
+
+    async delete_homework(homework_id) {
+        return axios.delete(`/homework/${homework_id}`);
+    },
+
+
+    async create_new_submission(student_id, homework_id) {
+        return axios.post('/submission', {
+            student_id: student_id,
+            homework_id: homework_id,
+        });
+    },
+
+    async get_submission(submission_id) {
+        return axios.get(`/submission/${submission_id}`);
+    },
+
+    async edit_submission(submission_id, points, fine) {
+        return axios.put(`/submission/${submission_id}`, {
+            points: points,
+            fine: fine,
+        });
+    },
+
+    async delete_submission(submission_id) {
+        return axios.delete(`/submission/${submission_id}`);
+    },
 }
