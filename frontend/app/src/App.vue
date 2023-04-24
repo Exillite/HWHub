5a49a6c18e406a14533c45f323f8e40b46cfacbd
Abignale <118287339+Abignale@users.noreply.github.com>
2023-04-24 12:35:32 +0300

    edited frontend

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <body>
     <div class="app">
       <router-view></router-view>
@@ -12,28 +11,6 @@ export default{
   components:{
   }
 }
-=======
-  <v-app>
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
-</template>
-
-<script>
-  import { useLocale } from 'vuetify'
-
-  export default {
-    setup () {
-      const { current } = useLocale()
-
-      return {
-        changeLocale: locale => current.value = locale
-      }
-    }
-  }
-
->>>>>>> a6e30e615dd691b7af8ccb08d2f53e38166c268f
 </script>
 <style>
 </style>
