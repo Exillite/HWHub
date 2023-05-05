2e22037cda72946700dc57ff596e761974e22256
Exillite <greencod67@gmail.com>
2023-05-05 10:56:39 +0300

    migrate homework module

--- backend/app/hwhub/modules/homework.py
+++ backend/app/hwhub/modules/homework.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, Depends
 
+from typing import Optional
+
 from .. import crud
 from .. import schemas
 from .. import validations
@@ -11,7 +13,8 @@ router = APIRouter(
     tags=["Homework"],
 )
 
-def check_permision(user: schemas.User, homework_id: str = None, student_group_id: str = None, perm="c") -> bool:
+
+async def check_permision(user: schemas.User, homework_id: Optional[str] = None, student_group_id: Optional[str] = None, perm="c") -> bool:
     """
     perm:
     c - create |
@@ -20,21 +23,31 @@ def check_permision(user: schemas.User, homework_id: str = None, student_group_i
     """
     if user.role == "admin":
         return True
-    
-    if perm == "c":
-        stg = crud.get_student_group(student_group_id)
-        return user.role in ["teacher"] and user.id == stg.teacher.id
-    if perm == "r":
-        hw = crud.get_homework(homework_id)
-        stg = crud.get_student_group(hw.student_group.pk)
-        for u_stg in user.students_groups:
-            if stg.pk == u_stg.pk:
-                return True
-        return stg.teacher.pk == user.id
-    if perm == "e":
-        hw = crud.get_homework(homework_id)
-        stg = crud.get_student_group(hw.student_group.pk)
-        return stg.teacher.pk == user.id
+
+    if perm == "c" and student_group_id:
+        stg = await crud.get_student_group(student_group_id)
+        if stg:
+            return user.role in ["teacher"] and user.id == stg.teacher.id
+    if perm == "r" and homework_id:
+        hw = await crud.get_homework(homework_id)
+        if hw:
+            if hw.student_group.id:
+                stg = await crud.get_student_group(hw.student_group.id)
+                if stg:
+                    for u_stg in user.students_groups:
+                        if stg.id == u_stg.id:
+                            return True
+                    return stg.teacher.id == user.id
+    if perm == "e" and homework_id:
+        hw = await crud.get_homework(homework_id)
+        if not hw:
+            return False
+        if not hw.student_group.id:
+            return False
+        stg = await crud.get_student_group(hw.student_group.id)
+        if not stg:
+            return False
+        return stg.teacher.id == user.id
     return False
 
 
@@ -43,10 +56,10 @@ async def create_new_homework(new_hw: schemas.HomeworkCreate, current_user: sche
     if not check_permision(current_user, student_group_id=new_hw.student_group_id):
         return {"status": 400}
     try:
-        hw = crud.create_homework(new_hw)
+        hw = await crud.create_homework(new_hw)
         if not hw:
             return {"status": 201}
-        return {"status": 200, "homework_id": str(hw.pk)}
+        return {"status": 200, "homework_id": str(hw.id)}
     except Exception as e:
         return {"status": 500, "error": str(e)}
 
@@ -56,7 +69,7 @@ async def get_homework(homework_id: str, current_user: schemas.User = Depends(au
     if not check_permision(current_user, homework_id=homework_id, perm="r"):
         return {"status": 400}
     try:
-        hw = crud.get_homework(homework_id)
+        hw = await crud.get_homework(homework_id)
         if not hw:
             return {"status": 201}
         return {"status": 200, "homework": hw.to_json()}
@@ -69,10 +82,10 @@ async def edit_homework(homework_id: str, edit_hw: schemas.HomeworkUpdate, curre
     if not check_permision(current_user, homework_id=homework_id, perm="e"):
         return {"status": 400}
     try:
-        hw = crud.edit_homework(edit_hw, homework_id)
+        hw = await crud.edit_homework(edit_hw, homework_id)
         if not hw:
             return {"status": 201}
-        return {"status": 200, "homework_id": str(hw.pk)}
+        return {"status": 200, "homework_id": str(hw.id)}
     except Exception as e:
         return {"status": 500, "error": str(e)}
 
@@ -82,7 +95,7 @@ async def delete_homework(homework_id: str, current_user: schemas.User = Depends
     if not check_permision(current_user, homework_id=homework_id, perm="e"):
         return {"status": 400}
     try:
-        crud.delete_homework(homework_id)
+        await crud.delete_homework(homework_id)
         return {"status": 200}
     except Exception as e:
         return {"status": 500, "error": str(e)}
@@ -93,8 +106,10 @@ async def get_homework_results(homework_id, current_user: schemas.User = Depends
     if not check_permision(current_user, homework_id=homework_id, perm="r"):
         return {"status": 400}
     try:
-        hw = crud.get_homework(homework_id)
-        subs = [sub.to_json() for sub in crud.get_submissions_by_homework(hw)]
+        hw = await crud.get_homework(homework_id)
+        if not hw:
+            return {"status": 201}
+        subs = [sub.to_json() for sub in await crud.get_submissions_by_homework(hw)]
         return {"status": 200, "submissions": subs}
     except Exception as e:
         return {"status": 500, "error": str(e)}
@@ -105,18 +120,21 @@ async def get_homework_marks(homework_id, current_user: schemas.User = Depends(a
     if not check_permision(current_user, homework_id=homework_id, perm="r"):
         return {"status": 400}
     try:
-        hw = crud.get_homework(homework_id)
-        subs = crud.get_submissions_by_homework(hw)
-        
-        resp = {"users": []}
-        
+        hw = await crud.get_homework(homework_id)
+        if not hw:
+            return {"status": 201}
+        subs = await crud.get_submissions_by_homework(hw)
+
+        resp = {"users": []}  # type: ignore
+
         for sub in subs:
-            resp["users"].append({"user": f"{sub.user.name} {sub.user.surname} {sub.user.patronymic}"})
+            resp["users"].append(
+                {"user": f"{sub.student.name} {sub.student.surname} {sub.student.patronymic}"})
             for i in range(len(sub.points)):
                 resp["users"][-1][i] = sub.points[i]
             resp["users"][-1]["fine"] = sub.fine
             resp["users"][-1]["mark"] = sub.mark
-            
+
         return {"status": 200, "marks": resp}
     except Exception as e:
         return {"status": 500, "error": str(e)}
