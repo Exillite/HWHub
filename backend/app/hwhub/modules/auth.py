d627ebba15fcc3ebcfd6ea9025205eb1e1dc7f9d
Exillite <greencod67@gmail.com>
2023-05-08 23:17:50 +0300

    format code

--- backend/app/hwhub/modules/auth.py
+++ backend/app/hwhub/modules/auth.py
@@ -11,7 +11,7 @@ from .. import schemas
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 14 # two weeks
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 14  # two weeks
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v0.1/auth/token")
--- frontend/app/vue.config.js
+++ frontend/app/vue.config.js
@@ -1,22 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
     transpileDependencies: true,
-    devServer: { allowedHosts: 'all', },
+    devServer: { allowedHosts: "all" },
     pluginOptions: {
-      moment: {
-        locales: [
-          ''
-        ]
-      },
-      vuetify: {},
-      i18n: {
-        locale: 'ru',
-        fallbackLocale: 'ru',
-        localeDir: 'locales',
-        enableLegacy: false,
-        runtimeOnly: false,
-        compositionOnly: false,
-        fullInstall: true
-      }
-    }
-})
+        moment: {
+            locales: [""],
+        },
+        vuetify: {},
+        i18n: {
+            locale: "ru",
+            fallbackLocale: "ru",
+            localeDir: "locales",
+            enableLegacy: false,
+            runtimeOnly: false,
+            compositionOnly: false,
+            fullInstall: true,
+        },
+    },
+});
