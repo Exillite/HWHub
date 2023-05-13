6b52958df0d240d70f10948c2612fd29a0b1ad3e
Exillite <greencod67@gmail.com>
2023-05-13 12:29:21 +0300

    add limit on mark

--- backend/app/hwhub/calculation.py
+++ backend/app/hwhub/calculation.py
@@ -16,5 +16,7 @@ def calculate_mark(hw_points, sub_points, formula, fine) -> float:
         [(CNT, points_count), (C, done_count), (K, sum_total_points)])
 
     mark = calc + fine
-    mark = round(mark, 2)
-    return float(mark)
+    mark = float(round(mark, 2))
+    if mark > 10: # TODO make max mark editable
+        mark = 10
+    return mark
