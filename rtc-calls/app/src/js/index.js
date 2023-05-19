0d32b63b7566cb3e205c901e08b7ddc630c465ec
Exillite <greencod67@gmail.com>
2023-05-19 12:39:48 +0300

    add call module

---
+++ rtc-calls/app/src/js/index.js
@@ -0,0 +1,335 @@
+function onopenMiro(share_link) {
+    // https://miro.com/app/board/uXjVMQ_om3w=/?share_link_id=870054768292
+    let lnk = share_link.split('/')
+    var is_next = false;
+    let board_id;
+    lnk.forEach(element => {
+        if (is_next) {
+            board_id = element
+            is_next = false
+        }
+
+        if (element === 'board') {
+            is_next = true
+        }
+    });
+
+    console.log(board_id)
+
+    var miroFrame = document.getElementById('miroFrame')
+    var miroInputFrame = document.getElementById('miroInputFrame')
+
+    // miroFrame.src = `https://miro.com/app/live-embed/${board_id}/?embedAutoplay=true`
+
+
+}
+
+onopenMiro('https://miro.com/app/board/uXjVMQ_om3w=/?share_link_id=870054768292')
+
+if (location.hash === '#init') {
+
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+        var Peer = require('simple-peer')
+        let is_camera = true;
+        let is_mic = true;
+        var peer = new Peer({
+            initiator: location.hash === '#init',
+            trickle: false,
+            stream: stream,
+        })
+        var ws = new WebSocket(`ws://${location.hostname}/api/ws/${location.search.split('=')[1]}`);
+
+
+        var localVideo = document.getElementById("localVideo")
+
+        if ('srcObject' in this) {
+            localVideo.srcObject = stream;
+        } else {
+            localVideo.srcObject = stream;
+        }
+
+        localVideo.play()
+
+        peer.on('signal', function(data) {
+            ws.send(JSON.stringify({ type: 'new_connection', data: JSON.stringify(data) }))
+        })
+
+        // document.getElementById('sendMessage').addEventListener('click', function() {
+        //     var yourMessage = document.getElementById('yourMessage').value
+        //     var rqst = {
+        //         type: 'chat-message',
+        //         is_init: location.hash === "#init",
+        //         text: yourMessage,
+        //     }
+        //     peer.send(JSON.stringify(rqst))
+
+        //     let chatArea = document.getElementById('chatArea')
+        //     var msg = document.createElement('div')
+        //     chatArea.appendChild(msg)
+        //     msg.innerHTML += yourMessage
+        //     msg.classList.add('message')
+        //     msg.classList.add('my-message')
+
+        //     chatArea.scrollTop = chatArea.scrollHeight;
+
+        //     document.getElementById('yourMessage').value = ''
+        // })
+
+        // document.getElementById('yourMessage').addEventListener("keydown", function(e) {
+        //     if (e.code === "Enter") {
+        //         var yourMessage = document.getElementById('yourMessage').value
+        //         var rqst = {
+        //             type: 'chat-message',
+        //             is_init: location.hash === "#init",
+        //             text: yourMessage,
+        //         }
+        //         peer.send(JSON.stringify(rqst))
+
+        //         let chatArea = document.getElementById('chatArea')
+        //         var msg = document.createElement('div')
+        //         chatArea.appendChild(msg)
+        //         msg.innerHTML += yourMessage
+        //         msg.classList.add('message')
+        //         msg.classList.add('my-message')
+
+        //         chatArea.scrollTop = chatArea.scrollHeight;
+
+        //         document.getElementById('yourMessage').value = ''
+        //     }
+        // })
+
+        peer.on('data', function(data) {
+            let rsp = JSON.parse(data)
+            let chatArea = document.getElementById('chatArea')
+            var msg = document.createElement('div')
+            chatArea.appendChild(msg)
+            msg.innerHTML += rsp.text
+            msg.classList.add('message')
+
+            chatArea.scrollTop = chatArea.scrollHeight;
+        })
+
+
+        document.getElementById('mutebtn').addEventListener('click', function() {
+            if (is_mic) {
+                stream.getTracks().forEach(function(track) {
+                    if (track.kind === "audio")
+                        track.enabled = false
+                });
+                document.getElementById('mutebtn').classList.add("footer-btn-off")
+            } else {
+                stream.getTracks().forEach(function(track) {
+                    if (track.kind === "audio")
+                        track.enabled = true
+                });
+                document.getElementById('mutebtn').classList.remove("footer-btn-off")
+            }
+            is_mic = !is_mic
+        })
+
+        document.getElementById('novideobtn').addEventListener('click', function() {
+            if (is_camera) {
+                stream.getTracks().forEach(function(track) {
+                    if (track.kind === "video")
+                        track.enabled = false
+                });
+                document.getElementById('novideobtn').classList.add("footer-btn-off")
+            } else {
+                stream.getTracks().forEach(function(track) {
+                    if (track.kind === "video")
+                        track.enabled = true
+                });
+                document.getElementById('novideobtn').classList.remove("footer-btn-off")
+            }
+            is_camera = !is_camera
+        })
+
+
+        peer.on('stream', function(stream) {
+            var video = document.getElementById("remouteVideo")
+
+            // video.src = window.URL.createObjectURL(stream)
+
+            if ('srcObject' in this) {
+                video.srcObject = stream;
+            } else {
+                video.srcObject = stream
+            }
+
+
+            video.play()
+        })
+
+        ws.onmessage = function(event) {
+            console.log(event.data)
+
+            let dt = event.data
+            dt = JSON.parse(dt)
+            if (dt.type === 'connect_final') {
+                peer.signal(dt.data)
+            }
+        };
+
+
+    }).catch((err) => {
+        console.log(err);
+    })
+} else {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+        var Peer = require('simple-peer')
+        let is_camera = true;
+        let is_mic = true;
+        var peer = new Peer({
+            initiator: location.hash === '#init',
+            trickle: false,
+            stream: stream,
+        })
+        var ws = new WebSocket(`ws://${location.hostname}/api/ws/${location.search.split('=')[1]}`);
+
+
+        var localVideo = document.getElementById("localVideo")
+
+        if ('srcObject' in this) {
+            localVideo.srcObject = stream;
+        } else {
+            localVideo.srcObject = stream;
+        }
+
+        localVideo.play()
+
+        peer.on('signal', function(data) {
+            ws.send(JSON.stringify({ type: 'connect', data: JSON.stringify(data) }))
+        })
+
+        // peer.on('data', function(data) {
+        //     document.getElementById('messages').textContent += data + '\n'
+        // })
+
+
+        // document.getElementById('sendMessage').addEventListener('click', function() {
+        //     var yourMessage = document.getElementById('yourMessage').value
+        //     var rqst = {
+        //         type: 'chat-message',
+        //         is_init: location.hash === "#init",
+        //         text: yourMessage,
+        //     }
+        //     peer.send(JSON.stringify(rqst))
+
+        //     let chatArea = document.getElementById('chatArea')
+        //     var msg = document.createElement('div')
+        //     chatArea.appendChild(msg)
+        //     msg.innerHTML += yourMessage
+        //     msg.classList.add('message')
+        //     msg.classList.add('my-message')
+
+        //     chatArea.scrollTop = chatArea.scrollHeight;
+
+        //     document.getElementById('yourMessage').value = ''
+        // })
+
+
+        // document.getElementById('yourMessage').addEventListener("keydown", function(e) {
+        //     if (e.code === "Enter") {
+        //         var yourMessage = document.getElementById('yourMessage').value
+        //         var rqst = {
+        //             type: 'chat-message',
+        //             is_init: location.hash === "#init",
+        //             text: yourMessage,
+        //         }
+        //         peer.send(JSON.stringify(rqst))
+
+        //         let chatArea = document.getElementById('chatArea')
+        //         var msg = document.createElement('div')
+        //         chatArea.appendChild(msg)
+        //         msg.innerHTML += yourMessage
+        //         msg.classList.add('message')
+        //         msg.classList.add('my-message')
+
+        //         chatArea.scrollTop = chatArea.scrollHeight;
+
+        //         document.getElementById('yourMessage').value = ''
+        //     }
+        // })
+
+        peer.on('data', function(data) {
+            let rsp = JSON.parse(data)
+            let chatArea = document.getElementById('chatArea')
+            var msg = document.createElement('div')
+            chatArea.appendChild(msg)
+            msg.innerHTML += rsp.text
+            msg.classList.add('message')
+
+            chatArea.scrollTop = chatArea.scrollHeight;
+        })
+
+
+
+        peer.on('stream', function(stream) {
+            var video = document.getElementById("remouteVideo")
+
+            // video.src = window.URL.createObjectURL(stream)
+
+            if ('srcObject' in this) {
+                video.srcObject = stream;
+            } else {
+                video.srcObject = stream
+            }
+
+            video.play()
+        })
+
+        document.getElementById('mutebtn').addEventListener('click', function() {
+            if (is_mic) {
+                stream.getTracks().forEach(function(track) {
+                    if (track.kind === "audio")
+                        track.enabled = false
+                });
+                document.getElementById('mutebtn').classList.add("footer-btn-off")
+            } else {
+                stream.getTracks().forEach(function(track) {
+                    if (track.kind === "audio")
+                        track.enabled = true
+                });
+                document.getElementById('mutebtn').classList.remove("footer-btn-off")
+            }
+            is_mic = !is_mic
+        })
+
+        document.getElementById('novideobtn').addEventListener('click', function() {
+            if (is_camera) {
+                stream.getTracks().forEach(function(track) {
+                    if (track.kind === "video")
+                        track.enabled = false
+                });
+                document.getElementById('novideobtn').classList.add("footer-btn-off")
+            } else {
+                stream.getTracks().forEach(function(track) {
+                    if (track.kind === "video")
+                        track.enabled = true
+                });
+                document.getElementById('novideobtn').classList.remove("footer-btn-off")
+            }
+            is_camera = !is_camera
+        })
+
+        ws.onmessage = function(event) {
+            console.log(event.data)
+                // console.log(event);
+
+            let dt = event.data
+            dt = JSON.parse(dt)
+            if (dt.type === 'connect_response') {
+                peer.signal(dt.data)
+            }
+        };
+
+        ws.onopen = (event) => {
+            ws.send(JSON.stringify({ type: 'connection_request' }))
+        }
+
+
+
+    }).catch((err) => {
+        console.log(err);
+    })
+}
---
+++ rtc-calls/app/src/js/ui.js
@@ -0,0 +1,66 @@
+let callTab = document.getElementById("callTab");
+let miroTab = document.getElementById("miroTab");
+
+let callPanel = document.getElementById("callPanel");
+let miroPanel = document.getElementById("miroPanel");
+
+let localVideo = document.getElementById("localVideo");
+let remoteVideo = document.getElementById("remoteVideo");
+
+let chatTab = document.getElementById("chatTab");
+let marksTab = document.getElementById("marksTab");
+
+let active_main_tab = callTab;
+let active_mini_tab = chatTab;
+
+function change_main_tab() {
+    active_main_tab.classList.remove("tab-active");
+    if (active_main_tab == callTab) {
+        active_main_tab = miroTab;
+
+        callPanel.hidden = true;
+        miroPanel.hidden = false;
+    } else if (active_main_tab == miroTab) {
+        active_main_tab = callTab;
+
+        miroPanel.hidden = true;
+        callPanel.hidden = false;
+    }
+    active_main_tab.classList.add("tab-active");
+}
+
+function change_mini_tab() {
+    active_mini_tab.classList.remove("tab-active");
+    if (active_mini_tab == chatTab) {
+        active_mini_tab = marksTab;
+    } else if (active_mini_tab == marksTab) {
+        active_mini_tab = chatTab;
+    }
+    active_mini_tab.classList.add("tab-active");
+}
+
+let left_panel = document.getElementById("leftPanel");
+let right_panel = document.getElementById("rightPanel");
+
+let is_leftPanel = false;
+
+function open_close_right_panel() {
+    if (is_leftPanel) {
+        right_panel.hidden = true;
+
+        left_panel.classList.remove("col-9");
+        left_panel.classList.add("col-10");
+
+        is_leftPanel = false;
+    } else {
+        left_panel.classList.remove("col-10");
+        left_panel.classList.add("col-9");
+
+        right_panel.hidden = false;
+        is_leftPanel = true;
+    }
+}
+
+document.getElementById("chatbtn").addEventListener("click", function() {
+    open_close_right_panel();
+});
