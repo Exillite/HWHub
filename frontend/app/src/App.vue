5399b47ef509eaf8c7075e93de1556c049be6304
Basique <basiqueevangelist@yandex.ru>
2023-08-08 01:18:42 +0300

    add logout button

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <v-toolbar-title @click="$router.push({ name: 'Main' })"
-        >ЛОГО</v-toolbar-title
-      >
+      <v-toolbar-title @click="$router.push({ name: 'Main' })">ЛОГО</v-toolbar-title>
       <v-spacer></v-spacer>
-      <router-link to="login" v-if="isSignedOut() && $route.name !== 'Login'">
+      <router-link to="login" v-if="isSignedOut() && $route.name !== 'Login'" :key="iHateVue">
         <v-btn text>
           Войти
         </v-btn>
       </router-link>
-      <router-link to="registration" v-if="isSignedOut() && $route.name !== 'Registration'">
+      <router-link to="registration" v-if="isSignedOut() && $route.name !== 'Registration'" :key="iHateVue">
         <v-btn text>
           Зарегистрироваться
         </v-btn>
       </router-link>
+      <v-btn text v-if="!isSignedOut()" @click="logout" :key="iHateVue">
+        Выйти
+      </v-btn>
     </v-app-bar>
     <v-main>
       <router-view />
@@ -35,5 +36,17 @@ export default {
       isSignedOut: () => !control.check_auth()
     };
   },
+  data() {
+    return {
+      iHateVue: 0
+    };
+  },
+  methods: {
+    logout() {
+      control.log_out()
+      this.iHateVue += 1;
+      this.$router.push({ name: "Login" });
+    }
+  }
 };
 </script>
